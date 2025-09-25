760fd0f855a9192de14c75a3d83fc0919b4fd903
Anthony <d.a.burchfield@att.net>
2025-09-24 21:42:07 -0500

    Added license notification at the top of CodegenOptionalSectionsTest.cs, ParserOptionalSectionsTests.cs and ParerOrderTests.cs.

--- Puma.Tests/ParserOrderTests.cs
+++ Puma.Tests/ParserOrderTests.cs
@@ -1,3 +1,20 @@
+// LLVM Compiler for the Puma programming language
+//   as defined in the document "The Puma Programming Language Specification"
+//   available at https://github.com/ThePumaProgrammingLanguage
+//
+// Copyright © 2024-2025 by Darryl Anthony Burchfield
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
