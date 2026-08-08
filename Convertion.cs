d41e6f5bb1cb4a3c7b285d47c79e46e1353fbd3d
Anthony <a.burchfield@att.net>
2026-08-08 18:28:14 -0500

    Added the  "WITH LLVM-exception" clause to the license.

--- Convertion.cs
+++ Convertion.cs
@@ -4,7 +4,7 @@
 //
 // Copyright © 2024-2026 by Darryl Anthony Burchfield
 //
-//   Licensed under the Apache License, Version 2.0 (the "License");
+//   Licensed under the Apache License, Version 2.0 (the "License") WITH LLVM-exception;
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
 //       http://www.apache.org/licenses/LICENSE-2.0
